d709f047c8ee8f27c1d763f0007f0e3142087ccd
h1k0n <ybzjyhk@gmail.com>
2020-04-05 14:40:27 +0800

    Update run_docker_redis.sh

--- run_docker_redis.sh
+++ run_docker_redis.sh
@@ -2,6 +2,8 @@
 
 docker run --name redis-two  -p 6379:6379 -d redis:4.0.10
 
+docker run --name redis-two  -p 6379:6379 -v /data/docker/redis/data:/data -d redis:4.0.10 --requirepass yourpassword  --appendonly yes
+
 docker run -p 6379:6379 -v /data/docker/redis/conf/redis.conf:/usr/local/etc/redis/redis.conf --name redis-two redis:4.0.10 redis-server /usr/local/etc/redis/redis.conf
 
 docker run -it --link redis-two:redis --rm redis:4.0.10 redis-cli -h redis -p 6379
