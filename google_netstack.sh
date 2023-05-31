06f93877e1d0305e0469b5cb419e5bf9905a51b4
h1k0n <ybzjyhk@gmail.com>
2023-05-31 09:33:24 +0800

    Update google_netstack.sh, gvisor netstack

--- google_netstack.sh
+++ google_netstack.sh
@@ -9,8 +9,10 @@ export GOPATH=~/go
 export GOROOT=/usr/local/go1.11.5
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 go get github.com/google/netstack/tcpip/sample/tun_tcp_echo
-ip tuntap add user $USER mode tun tap0
-ip link set tap0 up
-ip addr add 172.19.0.10/12 dev tap0
-#tun_tcp_echo tap0 172.19.0.4 3400
-#curl 172.19.0.4:3400 -v
+ip tuntap add user $USER mode tun tun5
+ip link set tun5 up
+ip addr add 192.168.57.10/12 dev tun5  //指定路由
+#tun_tcp_echo tap0 192.168.57.11 7000 //指定用户协议栈的echo服务地址
+#curl 192.168.57.11:7000 -v   //请求echo服务
+
+#新的tun_tcp_echo: https://github.com/google/gvisor/tree/master/pkg/tcpip/sample/tun_tcp_echo
