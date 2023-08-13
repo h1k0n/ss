0c0af519e2886126fba3a238bcaf3106b107d198
h1k0n <ybzjyhk@gmail.com>
2023-08-13 22:22:48 +0800

    Create latest-rust.nix

---
+++ latest-rust.nix
@@ -0,0 +1,35 @@
+/*
+based on
+https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
+*/
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  #rustVersion = "1.62.0";
+  rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+    targets = [ "riscv64imac-unknown-none-elf" ];
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "llvm-tools-preview"
+    ];
+  });
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    rust-analyzer
+    pkg-config
+    dtc
+    qemu
+    # other dependencies
+    #gtk3
+    #wrapGAppsHook
+    just
+  ]);
+  shellHook = ''
+    export PATH="$PATH:$HOME/.cargo/bin"
+  '';
+  RUST_BACKTRACE = 1;
+}
