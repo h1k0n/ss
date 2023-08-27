faa9a7906dd0a7f2fbba14c899b726ad5b7d9a09
h1k0n <ybzjyhk@gmail.com>
2023-08-27 12:53:34 +0800

    Update build-flash-mnemos.sh

--- build-flash-mnemos.sh
+++ build-flash-mnemos.sh
@@ -1,7 +1,10 @@
-//nix-shell latest-rust.nix
+# build github.com/jspngh/oreboot 
+# nix-shell nix-shell
+cargo make --release --variant lichee --supervisor #gen oreboot-nezha-bt0.bin
+sudo dd if=oreboot-nezha-bt0.bin of=/dev/sdb bs=1024 seek=8 conv=sync
+
+# nix-shell latest-rust.nix
 just build-d1 lichee-rv
 just build-d1 mq-pro
-
-sudo dd if=oreboot-nezha-bt0.bin of=/dev/sdb bs=1024 seek=8 conv=sync
 sudo dd if=mnemos-lichee-rv.bin of=/dev/sdb bs=1024 seek=40 conv=sync
 sudo dd if=mnemos-mq-pro.bin of=/dev/sdb bs=1024 seek=40 conv=sync
