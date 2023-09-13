ec3ac9f8589b8ce8b079c034b324f17d1e7ac153
h1k0n <ybzjyhk@gmail.com>
2023-09-13 20:38:22 +0800

    Update nix-upgrade.sh

--- nix-upgrade.sh
+++ nix-upgrade.sh
@@ -16,5 +16,7 @@ wget
 sudo nixos-rebuild switch --upgrade
 
 nix-channel --add https://nixos.org/channels/nixos-23.11 nixos
+#Channels are set per user. This means that running nix-channel --add as a non root user (or without sudo) will not affect configuration in /etc/nixos/configuration.nix
+nixos-rebuild switch --upgrade
 
 sudo nix-collect-garbage -d
