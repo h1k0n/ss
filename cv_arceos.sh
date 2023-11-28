488c1e9de035ed786558001303c6aef3a2015358
h1k0n <ybzjyhk@gmail.com>
2023-11-28 19:13:06 +0800

    Create cv_arceos.sh

---
+++ cv_arceos.sh
@@ -0,0 +1,5 @@
+# 从https://github.com/h888866j?tab=overview&from=2023-10-01&to=2023-10-31看出，h888866j向ZhiyuanSue/arceos_hsp提交代码
+# yuoo655直接拷贝了ZhiyuanSue的部分代码用于开发发送接收
+
+
+make A=apps/net/cvitek_nic_test ARCH=riscv64 SMP=1 NET=y LOG=info cv1811
