5ce2d713b8527f6dee28973cdf8544406271d014
h1k0n <ybzjyhk@gmail.com>
2023-11-29 12:03:08 +0800

    Update arceos_net.sh, 负责人

--- arceos_net.sh
+++ arceos_net.sh
@@ -1,3 +1,13 @@
+# 网卡驱动 https://github.com/orgs/rcore-os/discussions/30
+# 硬件支持 https://github.com/orgs/rcore-os/discussions/24
+# elliott10 Luoyuan Xiao 萧络元
+# zxgsn 张轩铬
+# yuoo655 CL  陈乐
+# ZhiyuanSue ZY
+# https://github.com/rcore-os/arceos/discussions/120
+# 星光派2驱动适配负责人: CL陈乐；
+# 华山派 + 通用PHY 的网络驱动负责人: ZY + RISCV开发群组爱好者；
+# T-HEAD C910开发板适配负责人: XLY萧络元
 #https://linuxhint.com/install_tftp_server_ubuntu/
 #设置网络接口网段　将itb文件放进~/tftp
 sudo ip addr add  192.168.157.2/24 dev  enx0826ae39b315
