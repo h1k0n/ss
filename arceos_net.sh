ab71e9f52b50875c2c75ecf7260f838b7c910ccc
h1k0n <ybzjyhk@gmail.com>
2023-11-30 22:11:01 +0800

    Update arceos_net.sh

--- arceos_net.sh
+++ arceos_net.sh
@@ -15,10 +15,10 @@
 #设置网络接口网段　将itb文件放进~/tftp
 sudo ip addr add  192.168.157.2/24 dev  enx0826ae39b315
 sudo ip link set enx0826ae39b315 up
-sudo ip addr add  192.168.157.2/24 dev  enx0826ae39b315
+sudo systemctl restart tftpd-hpa
 
 #typeC作为电源，uart控制，接入电源瞬间通过按任意按键停留在uboot状态
 setenv ipaddr 192.168.157.3	# 设置开发板ip
 setenv serverip 192.168.157.2	# 设置服务器ip
-tftpboot 0x82000000 192.168.157.3	# 内存只有80M
+tftpboot 0x82000000 arceos-cv1811.itb	# 内存只有80M
 bootm 0x82000000
